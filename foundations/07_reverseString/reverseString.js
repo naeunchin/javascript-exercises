ecd6acb9b0f7d80b5173ff9333348f413ed98387
naeunchin <naeunchin@gmail.com>
2026-09-09 14:42:04 -0600

    Foundations 07 complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = '';
 
+    // Approach 1 
+    // let reversedString = string.split('').reverse().join('');
+
+    // Approach 2
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
