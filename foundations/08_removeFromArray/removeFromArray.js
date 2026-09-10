cd6ea8fb2d91397cbee172e4c358db354cf7340b
naeunchin <naeunchin@gmail.com>
2026-09-10 14:18:12 -0600

    08 complete

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val));
+
+    // Another approach 
+    // const newArray = [];
+    // array.forEach((item) => {
+    //     if (!args.includes(item)) {
+    //         newArray.push(item);
+    //     }
+    // });
+
+    // return newArray;
 };
 
 // Do not edit below this line
