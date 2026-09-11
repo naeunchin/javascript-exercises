94eeea75c71975857514d797aca8d4ea81b3f3e0
naeunchin <naeunchin@gmail.com>
2026-09-10 20:23:21 -0600

    09 complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // Make sure the numbers are non-zero ints 
 
+    // Attempt 1: does not work because it breaks if num1 is bigger than num2
+    // if (num1 < 0 || num2 < 0) {
+    //     return 'Error';
+    // }
+    // let sum = 0; 
+    // for (let i = num1; i <= num2; i++) {
+    //     sum += i;
+    // }
+    // return sum;
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    // Get the lowest number from num1 and num2
+    if (num1 > num2) {
+        const placeholder = num1;
+        num1 = num2; 
+        num2 = placeholder;
+    }
+
+    let sum = 0;
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
