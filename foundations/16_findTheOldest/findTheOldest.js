f5b49be4c5083b2e57b409492a0654701e3c9ee9
naeunchin <naeunchin@gmail.com>
2026-09-15 21:10:24 -0600

    Foundations 16 complete - ALL COMPLETE

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Return the whole person object with the oldest person 
+    // Check the person's age: if yearOfDeath exists, then yod - yob OR if yod does not exist, currentYear - yob 
+    // Use JS Date function to get current year 
+    const currentYear = new Date().getFullYear();
+    
+    // Can use JS Array.prototype.reduce() and Array.prototype.sort(), but sort() reorders the entire collection (higher time complexity) 
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
 
+        return currentAge > oldestAge ? currentPerson : oldest;
+    });
 };
 
 // Do not edit below this line
